b9f10406613f7feb4ab29dd7ad91e682a8a6d9f7
Ben Norris <code@bsn.design>
2021-09-06 20:04:20 -0600

    Add script to add gallery thumbnail

---
+++ .utils/add-gallery-thumb.py
@@ -0,0 +1,40 @@
+import os
+import re
+
+def adjustFiles(directory):
+    for file in os.scandir(directory):
+        if file.is_file():
+            adjustFile(file)
+
+def adjustFile(file):
+    if file.path.__contains__('.DS_Store'):
+        return
+
+    image_regex = re.compile('(https://media.bennorris.org/images/.+?\.(jpg|png|jpeg))')
+    file_read = open(file, 'r', encoding='utf-8')
+    file_name = file_read.name
+    contents = file_read.read()
+    search = image_regex.search(contents)
+    file_read.close()
+
+    # If the post does not contain an image, no need to continue
+    if not search or not contents.__contains__('- sketchnotes'):
+        return
+
+    image_url = search.group(0)
+    thumb = 'gallery_thumb: ' + image_url + '\n'
+    adjusted_contents = re.sub('date:',thumb + 'date:',contents)
+
+    file_write = open(file, 'w')
+    file_write.write(adjusted_contents)
+    file_write.close()
+
+    print('added gallery thumbnail in '+file_name)
+
+def main():
+    directory = "_posts"
+    adjustFiles(directory)
+    # adjustFile('_posts/2020-05-05-star-wars-day.md')
+    # adjustFile('_posts/2021-08-11-linkedin-live-sketchnote.md')
+
+main()
