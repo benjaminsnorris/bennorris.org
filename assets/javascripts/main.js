1ff023d0b0819bc90b6d341092172bedc83e4bcb
Ben Norris <code@bsn.design>
2022-06-23 15:41:01 -0600

    Add javascript for plausible goals

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -74,3 +74,47 @@ function toggleClassSearch() {
     document.querySelector('.search-content input').focus();
   }, 400);
 }
+
+let buttons = document.querySelectorAll("button[data-analytics]");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', handleFormEvent);
+    buttons[i].addEventListener('auxclick', handleFormEvent);
+}
+
+function handleFormEvent(event) {
+    event.preventDefault();
+    let attributes = event.target.getAttribute('data-analytics').split(/,(.+)/);
+    let events = [JSON.parse(attributes[0]), JSON.parse(attributes[1] || '{}')];
+    plausible(...events);
+    setTimeout(function () {
+        event.target.form.submit();
+    }, 150);
+}
+
+let links = document.querySelectorAll("a[data-analytics]");
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', handleLinkEvent);
+    links[i].addEventListener('auxclick', handleLinkEvent);
+}
+
+function handleLinkEvent(event) {
+    var link = event.target;
+    var middle = event.type == "auxclick" && event.which == 2;
+    var click = event.type == "click";
+    while (link && (typeof link.tagName == 'undefined' || link.tagName.toLowerCase() != 'a' || !link.href)) {
+        link = link.parentNode;
+    }
+    if (middle || click) {
+        let attributes = link.getAttribute('data-analytics').split(/,(.+)/);
+        let events = [JSON.parse(attributes[0]), JSON.parse(attributes[1] || '{}')];
+        plausible(...events);
+    }
+    if (!link.target) {
+        if (!(event.ctrlKey || event.metaKey || event.shiftKey) && click) {
+            setTimeout(function () {
+                location.href = link.href;
+            }, 150);
+            event.preventDefault();
+        }
+    }
+}
